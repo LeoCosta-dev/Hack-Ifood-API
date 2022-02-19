047c497a800849a984dcadf730971101c056c579
LeoCosta-dev <leonardodacostadasilva@hotmail.com>
2022-02-19 13:42:18 -0300

    mudança de dados do mysql para json

--- src/app.js
+++ src/app.js
@@ -1,22 +1,13 @@
 const express = require("express");
-const connection = require("./infra/connection");
-const CreateTable = require("./infra/CreateTable");
-
+const bd = require("./infra/populateTable")
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-connection.connect((e) => {
-    if(e){
-        console.log(e)
-    } else {
-        CreateTable.init(connection)
-        app.listen(port, ()=>{
-            console.log(`Acesso ao servidor pelo endereço http://localhost:${port}`)
-        })
-        
-        app.get("/", (req, res)=>{
-            res.status(200).send("<h1>Paçoca</h1>")
-        })
-    }
+app.listen(port, ()=>{
+    console.log(`Acesso ao servidor pelo endereço http://localhost:${port}`)
+})
+
+app.get("/", (req, res) => {
+    res.status(200).json(bd)
 })
